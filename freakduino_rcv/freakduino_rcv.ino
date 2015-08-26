d1c817a3689ac32d799af3c101a37cabdbeaa4a1
Visar Ramku <visar.ramku@nyct.com>
2015-08-26 19:15:17 -0400

    added the ability to scroll through text lines (e.g. emergency msg, routes,etc); extended countdown timer for prediction; drop off stale data

--- freakduino_rcv/freakduino_rcv.ino
+++ freakduino_rcv/freakduino_rcv.ino
@@ -10,106 +10,183 @@
 #define FREAKDUINO_LONG_RANGE 1
 
 uint8_t AESKEY = 329093092;
-
-const int linelength = 16;
-const int maxlines = 2;
+char* dataReceived = "MTA Bus Time 1.1\nWaiting for data";
+boolean dataRCVD = false;
+unsigned long systemRunningTime = 0;
+unsigned short informationDropOffThreshold = 3; // 3 mins
+unsigned short informationUpdateRate = 4000; // 4 secs
 
 LiquidCrystal lcd(9, 8, 5, 4, 3, 2);
 
-void lcdprintstr(String data){
-    Serial.print("Writing to display: ");
-    Serial.println(data);
-    
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    //lcd.print(millis()/1000);
-    lcd.print(data.substring(0, 16));
-    lcd.setCursor(0, 1);
-    lcd.print(data.substring(16, 32));
-    //lcd.print(millis()/1000);
-    delay(100);
-}
-
 void setup()
 {
-  
+  //TODO: remove in production
   Serial.begin(9600);
-  
-  // Init the chibi wireless stack
+
   chibiInit();
   chibiSetChannel(CHANNEL);
   chibiAesInit(&AESKEY);
   chibiSetMode(BPSK_MODE);
   chibiSetDataRate(CHB_RATE_250KBPS);
-  chibiHighGainModeEnable();
-  
+  //  chibiHighGainModeEnable();
   chibiSetShortAddr(ME);
-  
   lcd.begin(16, 2);
-  
-  String initmsg = "MTA Bus Time 1.0Waiting for data";
-  lcdprintstr(initmsg);
-  
-  Serial.println("Ready!");
+  ProcessTextToLCD(dataReceived);
 }
+
 
 void loop()
 { 
-  //constantly receive
-  if (chibiDataRcvd() == true)
+  dataRCVD = isRCVD();
+
+  if (dataRCVD || millis() % informationUpdateRate == 0)
   {
-     byte rssi = chibiGetRSSI();
-     Serial.print("RSSI = "); 
-     Serial.println(rssi, HEX);
-      
-     byte buff[256];
-     chibiGetData(buff);
-      
-     char data[256];
-     strncpy(data, (char*) buff, 256);
-    
-     String temp = "";
-    
-     char* line = strtok(data, "\n");
-     while(line != 0){
-      int type = 0;//0 is minutes, 1 is stops
-      char* separator = strchr(line, '@');//@ is minute symbol, # is stops away symbol
-      if(separator == 0){
-        separator = strchr(line, '#');
-        type = 1;
-      }
-      
-      if(separator != 0){
-        //parse the information
-        *separator = 0;
-        Serial.print("Route: ");
-        Serial.print(line);
-        
-        ++separator;
-        int distance = atoi(separator);
-        
-        temp = temp + line + ": " + distance;
-        
-        if(type == 0){
-          Serial.print(" Minutes Away: ");
-          temp = temp + "min\n";
-        }else{
-          Serial.print(" Stops Away: ");
-          temp = temp + "stops\n";
-        }
-        
-        Serial.print(distance);
-        Serial.println();
-        
-      }
-      
-      line = strtok(0, "\n");
-    }
-    
-    String out = temp.substring(0, 16) + "-" + String(rssi, DEC) + "db up:" + (millis()/1000);
-    
-    lcdprintstr(out);
-    
+    ProcessTextToLCD(dataReceived);
+    Serial.println(dataReceived);
+  }
+  else if (!dataRCVD && ((millis() - systemRunningTime)/60000) % informationDropOffThreshold == 0 )
+  {
+    dataReceived = (char*) "MTA Bus Time 1.2\nWaiting for data";
   }
 
 }
+/*
+  Fetching Bus Time Data
+  
+*/
+boolean isRCVD()
+{ 
+  if (chibiDataRcvd() == true)
+  {
+    byte buff[256];
+    chibiGetData(buff);
+    systemRunningTime = millis();
+    dataReceived = (char*) buff;
+    // free (buff);
+    return true;
+  }
+  return false;
+}
+
+/*
+   Processing Text to LCD
+*/
+void ProcessTextToLCD(char* dataRecived)
+{
+  char* dataRecivedCopy = (char*) malloc(strlen(dataRecived) + 1); 
+  strcpy(dataRecivedCopy, dataRecived);
+  unsigned short int count = 0;
+
+  char * line = strtok(dataRecived, "\n"); 
+  while(line != NULL)
+  {
+    if(count == 2)
+    {
+      count = 0;
+      delay(informationUpdateRate/2);
+    }
+
+    updateLCDScreen(getArrival(line), count);  
+    line = strtok(0, "\n");
+    ++count;
+  }
+
+  strcpy(dataRecived,dataRecivedCopy);
+  free(dataRecivedCopy);
+}
+
+/*
+  Process Bus Time Text
+*/
+
+String getArrival(char* busArrival)
+{     
+  //@ Defines Minutes Away
+  //# Defines Stops Away
+  
+  String temp = "";
+
+  if(char * separator = strchr(busArrival, '@'))
+  {
+    *separator = 0;
+    temp = temp + busArrival+":";
+
+    ++separator;
+    if(!dataRCVD){
+      int arrivalTime = atoi(separator);
+      arrivalTime = countDownTime(arrivalTime);
+      temp = temp + arrivalTime+ "mins*";
+    }
+    else{
+      temp = temp + separator+ "mins";
+    }
+  }
+  else if (char * separator = strchr(busArrival, '#'))
+  {
+    *separator = 0;
+    ++separator;
+    temp = temp + busArrival+":"+separator + "stops";     
+
+  }
+  else{
+    temp = temp + busArrival;
+  }
+
+  return temp;
+}
+/*
+  Count Down for stale data
+*/
+int countDownTime(int input)
+{
+  int staleDataMinutes = (millis()-systemRunningTime)/60000;
+  
+  if(staleDataMinutes > input)
+    return 0;
+  else
+    return (input - staleDataMinutes);
+}
+
+/*
+  Update LCD screen by a given row
+*/
+void updateLCDScreen(String Line, unsigned short int row)
+{
+  char clsChar[17] = "                ";
+  lcd.setCursor(0, row);
+  lcd.print(clsChar);
+  lcd.setCursor(0, row);
+  lcd.print(Line);
+
+}
+
+
+// if needed --> Append Char Function 
+/*
+char* appendCharPointer(char* dest, char src[])
+ {
+ size_t len_dest = strlen(dest);
+ size_t len_src = strlen(src);
+ 
+ char* ret = new char[len_dest+len_src];
+ strcpy(ret, len_src);
+ for( int i = 0; i < len_src; i++)
+ {
+ ret[len_dest+i]=a[i];
+ }  
+ ret[len_dest+1] = '\0';
+ 
+ return ret;
+ }
+ */
+
+
+
+
+
+
+
+
+
+
+
